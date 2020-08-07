dc7827552c160934a916e484b12c8b3127cac0fc
joy <1824600831@qq.com>
2020-08-07 16:20:27 +0800

    第二次demo2修改的哦

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,7 @@
 public class Hello{
  public static void main(String[] args){
-	System.out.pringtln("hello,终于成功啦")
+	System.out.pringtln("hello,终于成功啦")；
+	System.out.pringtln("hello,第二次终于成功啦")
 }
 
 }
