2cccead3e8f2e68830133d4aeda7b06135d6422c
joy <1824600831@qq.com>
2020-08-07 16:33:17 +0800

    demo2最后一次修改

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,8 @@ public class Hello{
  public static void main(String[] args){
 	System.out.pringtln("hello,终于成功啦")；
 	System.out.pringtln("hello,第二次终于成功啦")
+
+	System.out.pringtln("hello,demo2最后一次修改终于成功啦")
 }
 
 }
