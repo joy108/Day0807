a6ad0a5d5bda0f57c81ddc84de7733f565b5f5f2
joy <1824600831@qq.com>
2020-08-07 16:25:34 +0800

    这个是demo1修改的哦

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,7 @@ public class Hello{
  public static void main(String[] args){
 	System.out.pringtln("hello,终于成功啦")；
 	System.out.pringtln("hello,第二次终于成功啦")
+System.out.pringtln("hello,demo1修改了哦，终于成功啦")
 }
 
 }
